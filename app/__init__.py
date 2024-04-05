61e642c126a85e14c84163637c036550508dcc77
pch2103 <pch2103@gmail.com>
2024-04-05 19:05:56 +0300

    Change-1

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,8 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+from app.errors import bp as errors_bp
+
+app.register_blueprint(errors_bp)  #Register Blueprints
+
 from app import routes
